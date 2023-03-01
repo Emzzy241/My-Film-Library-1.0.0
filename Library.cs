d20cbfe569016f5d9d34fdb8686fa22ad9d27aa8
Emzzy241 <emzzyoluwole@gmail.com>
2023-03-01 15:43:05 +0100

    adding my C# code file

---
+++ Library.cs
@@ -0,0 +1,27 @@
+// GOALS set out for this APP
+// Everyone likes IMDB... Now I want to make a little film library database of my own
+// This will be a desktop App about movies I own/love/want to watch.
+// At the finished Product of this App, the App should be able to:
+// * Store data on a new movie
+// * Retrieve data on a movie that exists in the library
+// * Include search functionality and the ability to edit entries
+
+// You can use Microsoft SQL Server for the database and Visual Studio for the front end. Add in other Amazing features you want
+// Maybe I might add a feature that could pull data from IMDB to poppulate my application. 
+// Other features could also pull images, movie posters and trailers from the web.
+
+
+using System;
+using System.Collections.Generic;
+
+// Don't forget I am using the class System.Collections.Generic namespace because I want my Console App to be able to access Dictionaries in it
+
+class Library{
+    static void Main(){
+        Console.WriteLine("WELCOME aboard to MY FILM LIBRARY 1.0.1");
+        Console.WriteLine("This is an App whose aim is to give you the best when it comes to of Film Libraries");
+        Console.WriteLine();
+
+
+    }
+}
